ff145b013cf01d6260b4c96826eeae1001f709a4
Bartlomiej Nowak <bartlomiej.nwk@gmail.com>
2024-09-27 19:38:12 +0200

    Compact case statements using a non-standard formatting

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -86,8 +86,8 @@ void PPU::execute(uint8_t cycles) {
             case 209:   case 217:   case 225:   case 233:   case 241:
             case 249:   case 257:   case 265:   case 273:   case 281:
             case 289:   case 297:   case 305:   case 313:   case 321:
-            case 329:   case 337:   case 339: 
-            // clang-format on
+            case 329:   case 337:   case 339:
+                // clang-format on
                 bus.addr = 0x2000 | (v.addr & 0x0FFF);
                 break;
             // NT read
@@ -101,244 +101,82 @@ void PPU::execute(uint8_t cycles) {
             case 242:   case 250:   case 258:   case 266:   case 274:
             case 282:   case 290:   case 298:   case 306:   case 314:
             case 322:   case 330:   case 338:   case 340:
-            // clang-format on
+                // clang-format on
                 nt = read(bus.addr);
                 break;
             // AT fetch
             // clang-format off
-            case 3:
-            case 11:
-            case 19:
-            case 27:
-            case 35:
-            case 43:
-            case 51:
-            case 59:
-            case 67:
-            case 75:
-            case 83:
-            case 91:
-            case 99:
-            case 107:
-            case 115:
-            case 123:
-            case 131:
-            case 139:
-            case 147:
-            case 155:
-            case 163:
-            case 171:
-            case 179:
-            case 187:
-            case 195:
-            case 203:
-            case 211:
-            case 219:
-            case 227:
-            case 235:
-            case 243:
-            case 251:
-            case 323:  // Two tiles from next scanline
-            case 331:  // Two tiles from next scanline
-            // clang-format on
+            case 3:     case 11:    case 19:    case 27:    case 35:
+            case 43:    case 51:    case 59:    case 67:    case 75:
+            case 83:    case 91:    case 99:    case 107:   case 115:
+            case 123:   case 131:   case 139:   case 147:   case 155:
+            case 163:   case 171:   case 179:   case 187:   case 195:
+            case 203:   case 211:   case 219:   case 227:   case 235:
+            case 243:   case 251:   case 323:   case 331:
+                // clang-format on
                 bus.addr = 0x23C0 | (v.addr & 0x0C00) | ((v.addr >> 4) & 0x38) |
                            ((v.addr >> 2) & 0x07);
                 break;
             // AT read
             // clang-format off
-            case 4:
-            case 12:
-            case 20:
-            case 28:
-            case 36:
-            case 44:
-            case 52:
-            case 60:
-            case 68:
-            case 76:
-            case 84:
-            case 92:
-            case 100:
-            case 108:
-            case 116:
-            case 124:
-            case 132:
-            case 140:
-            case 148:
-            case 156:
-            case 164:
-            case 172:
-            case 180:
-            case 188:
-            case 196:
-            case 204:
-            case 212:
-            case 220:
-            case 228:
-            case 236:
-            case 244:
-            case 252:
-            case 324:  // Two tiles from next scanline
-            case 332:  // Two tiles from next scanline
-            // clang-format on
+            case 4:     case 12:    case 20:    case 28:    case 36:
+            case 44:    case 52:    case 60     case 68:    case 76:
+            case 84:    case 92:    case 100:   case 108:   case 116:
+            case 124:   case 132:   case 140:   case 148:   case 156:
+            case 164:   case 172:   case 180:   case 188:   case 196:
+            case 204:   case 212:   case 220:   case 228:   case 236:
+            case 244:   case 252:   case 324:   case 332:
+                // clang-format on
                 at = read(bus.addr);
                 break;
             // BG l fetch
             // clang-format off
-            case 5:
-            case 13:
-            case 21:
-            case 29:
-            case 37:
-            case 45:
-            case 53:
-            case 61:
-            case 69:
-            case 77:
-            case 85:
-            case 93:
-            case 101:
-            case 109:
-            case 117:
-            case 125:
-            case 133:
-            case 141:
-            case 149:
-            case 157:
-            case 165:
-            case 173:
-            case 181:
-            case 189:
-            case 197:
-            case 205:
-            case 213:
-            case 221:
-            case 229:
-            case 237:
-            case 245:
-            case 253:
-            case 325:  // Two tiles from next scanline
-            case 333:  // Two tiles from next scanline
-            // clang-format on
+            case 5:     case 13:    case 21:    case 29:    case 37:
+            case 45:    case 53:    case 61:    case 69:    case 77:
+            case 85:    case 93:    case 101:   case 109:   case 117:
+            case 125:   case 133:   case 141:   case 149:   case 157:
+            case 165:   case 173:   case 181:   case 189:   case 197:
+            case 205:   case 213:   case 221:   case 229:   case 237:
+            case 245:   case 253:   case 325:   case 333:
+                // clang-format on
                 bus.addr = (ppuctrl.spr_pt_addr ? 0x1000 : 0x0000) + nt * 16 +
                            v.sc_fine_y;
                 break;
-            // BG l read 
+            // BG l read
             // clang-format off
-            case 6:
-            case 14:
-            case 22:
-            case 30:
-            case 38:
-            case 46:
-            case 54:
-            case 62:
-            case 70:
-            case 78:
-            case 86:
-            case 94:
-            case 102:
-            case 110:
-            case 118:
-            case 126:
-            case 134:
-            case 142:
-            case 150:
-            case 158:
-            case 166:
-            case 174:
-            case 182:
-            case 190:
-            case 198:
-            case 206:
-            case 214:
-            case 222:
-            case 230:
-            case 238:
-            case 246:
-            case 254:
-            case 326:  // Two tiles from next scanline
-            case 334:  // Two tiles from next scanline
-            // clang-format on
+            case 6:     case 14:    case 22:    case 30:    case 38:
+            case 46:    case 54:    case 62:    case 70:    case 78:
+            case 86:    case 94:    case 102:   case 110:   case 118:
+            case 126:   case 134:   case 142:   case 150:   case 158:
+            case 166:   case 174:   case 182:   case 190:   case 198:
+            case 206:   case 214:   case 222:   case 230:   case 238:
+            case 246:   case 254:   case 326:   case 334:
+                // clang-format on
                 spr_l = read(bus.addr);
                 break;
             // BG h fetch
             // clang-format off
-            case 7:
-            case 15:
-            case 23:
-            case 31:
-            case 39:
-            case 47:
-            case 55:
-            case 63:
-            case 71:
-            case 79:
-            case 87:
-            case 95:
-            case 103:
-            case 111:
-            case 119:
-            case 127:
-            case 135:
-            case 143:
-            case 151:
-            case 159:
-            case 167:
-            case 175:
-            case 183:
-            case 191:
-            case 199:
-            case 207:
-            case 215:
-            case 223:
-            case 231:
-            case 239:
-            case 247:
-            case 255:
-            case 327:  // Two tiles from next scanline
-            case 335:  // Two tiles from next scanline
-            // clang-format on
+            case 7:     case 15:    case 23:    case 31:    case 39:
+            case 47:    case 55:    case 63:    case 71:    case 79:
+            case 87:    case 95:    case 103:   case 111:   case 119:
+            case 127:   case 135:   case 143:   case 151:   case 159:
+            case 167:   case 175:   case 183:   case 191:   case 199:
+            case 207:   case 215:   case 223:   case 231:   case 239:
+            case 247:   case 255:   case 327:   case 335:
+                // clang-format on
                 bus.addr = (ppuctrl.spr_pt_addr ? 0x1000 : 0x0000) + nt * 16 +
                            v.sc_fine_y + 8;
                 break;
             // BG h read
             // clang-format off
-            case 8:
-            case 16:
-            case 24:
-            case 32:
-            case 40:
-            case 48:
-            case 56:
-            case 64:
-            case 72:
-            case 80:
-            case 88:
-            case 96:
-            case 104:
-            case 112:
-            case 120:
-            case 128:
-            case 136:
-            case 144:
-            case 152:
-            case 160:
-            case 168:
-            case 176:
-            case 184:
-            case 192:
-            case 200:
-            case 208:
-            case 216:
-            case 224:
-            case 232:
-            case 240:
-            case 248:
-            case 256:
-            case 328:  // Two tiles from next scanline
-            case 336:  // Two tiles from next scanline
-            // clang-format on
+            case 8:     case 16:    case 24:    case 32:    case 40:
+            case 48:    case 56:    case 64:    case 72:    case 80:
+            case 88:    case 96:    case 104:   case 112:   case 120:
+            case 128:   case 136:   case 144:   case 152:   case 160:
+            case 168:   case 176:   case 184:   case 192:   case 200:
+            case 208:   case 216:   case 224:   case 232:   case 240:
+            case 248:   case 256:   case 328:   case 336:
+                // clang-format on
                 spr_h = read(bus.addr);
                 if (scan_y == 256) {
                     inc_vert(v);
